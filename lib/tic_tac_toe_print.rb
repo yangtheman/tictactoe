583be42ead70be8b53fb1020cf97354a6fbb5f6d
Yang Chung <yangtheman@gmail.com>
2014-03-19 23:35:08 -0700

    Use x_coords and y_coords methods instead of constants

--- lib/tic_tac_toe_print.rb
+++ lib/tic_tac_toe_print.rb
@@ -5,17 +5,17 @@ class TicTacToePrint
     def print_board(game)
       (0).upto(3) do |row_num|
         if row_num == 0
-          puts ([" "] + TicTacToe::X_COORDS).join(" | ")
+          puts ([" "] + game.x_coords).join(" | ")
         else
           y_index = row_num -1
-          puts ([TicTacToe::Y_COORDS[y_index]] + get_values_of_row(y_index, game)).join(" | ")
+          puts ([game.y_coords[y_index]] + get_values_of_row(y_index, game)).join(" | ")
         end
       end
     end
     
     def get_values_of_row(y_index, game)
-      row = TicTacToe::Y_COORDS[y_index]
-      TicTacToe::X_COORDS.inject([]) do |array, x|
+      row = game.y_coords[y_index]
+      game.x_coords.inject([]) do |array, x|
         array << game.cell_marker(x, row)
         array
       end
